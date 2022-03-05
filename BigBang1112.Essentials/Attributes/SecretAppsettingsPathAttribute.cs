5741503fc625a4958707bb5d12e87bc44b10357c
Petr Pivoňka <petrpiv1@gmail.com>
2022-03-05 17:49:23 +0100

    Add SecretAppsettingsPathAttribute

---
+++ BigBang1112.Essentials/Attributes/SecretAppsettingsPathAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace BigBang1112.Attributes;
+
+public class SecretAppsettingsPathAttribute : Attribute
+{
+    public string Path { get; }
+
+    public SecretAppsettingsPathAttribute(string path)
+    {
+        Path = path;
+    }
+}
