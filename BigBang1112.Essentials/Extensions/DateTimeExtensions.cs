9bab06fd9eb438f7fa04123c74125946c02cfcf2
Petr Pivoňka <petrpiv1@gmail.com>
2022-03-12 13:47:44 +0100

    Add ToTimestampTag

--- BigBang1112.Essentials/Extensions/DateTimeExtensions.cs
+++ BigBang1112.Essentials/Extensions/DateTimeExtensions.cs
@@ -1,9 +1,16 @@
-﻿namespace BigBang1112.Extensions;
+﻿using Discord;
+
+namespace BigBang1112.Extensions;
 
 public static class DateTimeExtensions
 {
     public static long ToUnix(this DateTime dateTime)
     {
         return new DateTimeOffset(DateTime.SpecifyKind(dateTime, DateTimeKind.Utc)).ToUnixTimeSeconds();
+    }
+
+    public static TimestampTag ToTimestampTag(this DateTime dateTime, TimestampTagStyles style = TimestampTagStyles.ShortDateTime)
+    {
+        return TimestampTag.FromDateTime(DateTime.SpecifyKind(dateTime, DateTimeKind.Utc), style);
     }
 }
