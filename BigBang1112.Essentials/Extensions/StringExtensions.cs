c6130170db514f9a4c8ae2f78d54d686a2c423c9
Petr Pivoňka <petrpiv1@gmail.com>
2022-03-20 12:44:11 +0100

    Add EscapeDiscord

---
+++ BigBang1112.Essentials/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace BigBang1112.Extensions;
+
+public static class StringExtensions
+{
+    public static string EscapeDiscord(this string str)
+    {
+        return str.Replace("_", "\\_").Replace("*", "\\*");
+    }
+}
