2efae1dc4836553b571e8c5c04ecc5a684235fbe
Petr Pivoňka <petrpiv1@gmail.com>
2022-04-02 17:24:19 +0200

    Improve EscapeDiscord

---
+++ BigBang1112.Essentials.Tests/Unit/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using BigBang1112.Extensions;
+using Xunit;
+
+namespace BigBang1112.Tests.Unit.Extensions;
+
+public class StringExtensionsTests
+{
+    // test EscapeDiscord
+    [Fact]
+    public void EscapeDiscord_Formatted_ReturnsEscapedString()
+    {
+        // arrange
+        var input = "**This** *is* _a test_ ||string||";
+        var expected = "\\*\\*This\\*\\* \\*is\\* \\_a test\\_ \\|\\|string\\|\\|";
+
+        // act
+        var actual = input.EscapeDiscord();
+
+        // assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void EscapeDiscord_NotFormatted_ReturnsSameString()
+    {
+        // arrange
+        var input = "This is a test string";
+        var expected = input;
+
+        // act
+        var actual = input.EscapeDiscord();
+
+        // assert
+        Assert.Equal(expected, actual);
+    }
+}
