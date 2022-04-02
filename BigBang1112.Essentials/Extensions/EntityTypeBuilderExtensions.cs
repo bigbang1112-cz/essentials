c68079c7044b90096df02330a2f080d086017fed
Petr Pivoňka <petrpiv1@gmail.com>
2022-04-02 20:39:32 +0200

    Add HasEnumData

---
+++ BigBang1112.Essentials/Extensions/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using BigBang1112.Exceptions;
+using BigBang1112.Models.Db;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection;
+
+namespace BigBang1112.Extensions;
+
+public static class EntityTypeBuilderExtensions
+{
+    public static DataBuilder<TEntity> HasEnumData<TEntity, TEnum, TAttribute>(this EntityTypeBuilder<TEntity> entityTypeBuilder,
+        Func<TAttribute, TEntity> seed)
+        where TEntity : DbModel where TEnum : struct, Enum where TAttribute : Attribute
+    {
+        return entityTypeBuilder.HasData(Enum.GetValues<TEnum>().Select(game =>
+        {
+            var fieldInfo = typeof(TEnum).GetField(game.ToString()) ?? throw new ThisShouldNotHappenException("Field doesn't exist even though it should");
+            var att = fieldInfo.GetCustomAttribute<TAttribute>() ?? throw new AttributeMissingException("GameAttribute missing");
+            var model = seed(att);
+            model.Id = (int)(object)game;
+            return model;
+        }));
+    }
+}
