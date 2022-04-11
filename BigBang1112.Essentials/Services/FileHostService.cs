b187e3c18f88f0403f44686b3130cbf1e495182d
Petr Pivoňka <petrpiv1@gmail.com>
2022-04-11 16:26:51 +0200

    Add API file save features

--- BigBang1112.Essentials/Services/FileHostService.cs
+++ BigBang1112.Essentials/Services/FileHostService.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.FileProviders;
+using System.IO.Compression;
+using System.Text.Json;
 
 namespace BigBang1112.Services;
 
 public class FileHostService : IFileHostService
 {
+    private const string JsonExtension = "json.gz";
+    
     private readonly IWebHostEnvironment _env;
+    
+    private static readonly JsonSerializerOptions jsonSerializerOptions = new(JsonSerializerDefaults.Web);
 
     public FileHostService(IWebHostEnvironment env)
     {
@@ -39,5 +45,61 @@ public class FileHostService : IFileHostService
     public IFileInfo GetFileInfo(string subpath)
     {
         return _env.WebRootFileProvider.GetFileInfo(subpath);
+    }
+
+    public void SaveToApi<T>(T jsonObject, int apiVersion, string fullFileNameWithoutExtension)
+    {
+        using var gzip = OpenStream_SaveJsonObjectToApi(apiVersion, fullFileNameWithoutExtension);
+        JsonSerializer.Serialize(gzip, jsonObject, jsonSerializerOptions);
+    }
+
+    public async Task SaveToApiAsync<T>(T jsonObject, int apiVersion, string fullFileNameWithoutExtension, CancellationToken cancellationToken = default)
+    {
+        using var gzip = OpenStream_SaveJsonObjectToApi(apiVersion, fullFileNameWithoutExtension);
+        await JsonSerializer.SerializeAsync(gzip, jsonObject, jsonSerializerOptions, cancellationToken);
+    }
+
+    private GZipStream OpenStream_SaveJsonObjectToApi(int apiVersion, string fullFileNameWithoutExtension)
+    {
+        var finalPath = GetApiPath(apiVersion, fullFileNameWithoutExtension, JsonExtension);
+        return new GZipStream(File.Create(finalPath), CompressionMode.Compress);
+    }
+
+    public void SaveToApi(Stream stream, int apiVersion, string fullFileNameWithExtension)
+    {
+        using var fs = OpenStream_SaveStreamToApi(apiVersion, fullFileNameWithExtension);
+        stream.CopyTo(fs);
+    }
+
+    public async Task SaveToApiAsync(Stream stream, int apiVersion, string fullFileNameWithExtension, CancellationToken cancellationToken = default)
+    {
+        using var fs = OpenStream_SaveStreamToApi(apiVersion, fullFileNameWithExtension);
+        await stream.CopyToAsync(fs, cancellationToken);
+    }
+
+    private FileStream OpenStream_SaveStreamToApi(int apiVersion, string fullFileNameWithExtension)
+    {
+        var finalPath = GetApiPath(apiVersion, fullFileNameWithExtension);
+        return File.Create(finalPath);
+    }
+
+    public string GetApiPath(int apiVersion, string fullFileNameWithoutExtension, string extension)
+    {
+        return Path.Combine(GetWebRootPath(), "api", $"v{apiVersion}", $"{fullFileNameWithoutExtension}.{extension}");
+    }
+
+    public string GetApiPath(int apiVersion, string fullFileNameWithExtension)
+    {
+        return Path.Combine(GetWebRootPath(), "api", $"v{apiVersion}", fullFileNameWithExtension);
+    }
+
+    public bool ExistsInApi(int apiVersion, string fullFileNameWithoutExtension, string extension)
+    {
+        return File.Exists(GetApiPath(apiVersion, fullFileNameWithoutExtension, extension));
+    }
+
+    public bool JsonExistsInApi(int apiVersion, string fullFileNameWithoutExtension)
+    {
+        return File.Exists(GetApiPath(apiVersion, fullFileNameWithoutExtension, JsonExtension));
     }
 }
--- BigBang1112.Essentials/Services/IFileHostService.cs
+++ BigBang1112.Essentials/Services/IFileHostService.cs
@@ -7,4 +7,15 @@ public interface IFileHostService
     IFileInfo GetFileInfo(string subpath);
     string GetFilePath(string folder, string fileName);
     string GetWebRootPath();
+
+    string GetDirectoryName(string fullFileName)
+    {
+        return Path.GetDirectoryName(fullFileName) ?? throw new Exception();
+    }
+
+    void SaveToApi<T>(T jsonObject, int apiVersion, string fullFileNameWithoutExtension);
+    Task SaveToApiAsync<T>(T jsonObject, int apiVersion, string fullFileNameWithoutExtension, CancellationToken cancellationToken = default);
+    void SaveToApi(Stream stream, int apiVersion, string fullFileNameWithExtension);
+    Task SaveToApiAsync(Stream stream, int apiVersion, string fullFileNameWithExtension, CancellationToken cancellationToken = default);
+    string GetApiPath(int apiVersion, string fullFileNameWithoutExtension, string extension);
 }
