fca1df07f243e4820cf470e0e54d35202f510ef8
Petr Pivoňka <petrpiv1@gmail.com>
2022-04-13 02:06:13 +0200

    Use Serilog

---
+++ BigBang1112.Essentials/Extensions/HostBuilderEssentialsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using BigBang1112.Serilog.Enrichers;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace BigBang1112.Extensions;
+
+public static class HostBuilderEssentialsExtensions
+{
+    public static IHostBuilder UseEssentials(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.UseSerilog((context, config) =>
+        {
+            config.ReadFrom.Configuration(context.Configuration);
+            config.Enrich.With<ServiceNameEnricher>();
+            config.WriteTo.File("./logs/log-.txt", rollingInterval: RollingInterval.Day);
+            config.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3} {ServiceName}] {Message:lj}{NewLine}{Exception}");
+        });
+
+        return hostBuilder;
+    }
+}
---
+++ BigBang1112.Essentials/Serilog/Enrichers/ServiceNameEnricher.cs
@@ -0,0 +1,25 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace BigBang1112.Serilog.Enrichers;
+
+public class ServiceNameEnricher : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        _ = logEvent ?? throw new ArgumentNullException(nameof(logEvent));
+        _ = propertyFactory ?? throw new ArgumentNullException(nameof(propertyFactory));
+
+        if (logEvent.Properties.TryGetValue("SourceContext", out var propertyValue))
+        {
+            if (propertyValue is not ScalarValue scalarValue || scalarValue.Value is not string value)
+            {
+                return;
+            }
+
+            var serviceName = value[(value.LastIndexOf('.') + 1)..];
+
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("ServiceName", serviceName));
+        }
+    }
+}
