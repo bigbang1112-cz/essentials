acf7a662273dca2cb6d410e26b5874f806179c90
Petr Pivoňka <petrpiv1@gmail.com>
2022-05-16 02:54:50 +0200

    Add wait.js

---
+++ BigBang1112.Essentials.Razor/wwwroot/js/wait.js
@@ -0,0 +1,7 @@
+﻿window.setInterval(function () {
+    var wait = document.getElementById("wait");
+    if (wait.innerHTML.length >= 3)
+        wait.innerHTML = "";
+    else
+        wait.innerHTML += ".";
+}, 500);
